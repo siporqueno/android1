5dceae08f0edf1fabab796bdc1b0d383ad363247
siporqueno <strike1000001@gmail.com>
2019-01-22 13:04:05 +0300

    Added 3 TextViews to show place (city and country code), temperature and type of weather. Added a button to refresh the above parameters.

--- app/src/main/java/com/example/lessonone/MainActivity.java
+++ app/src/main/java/com/example/lessonone/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.lessonone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int Temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView place = findViewById(R.id.textView1);
+        place.setText(R.string.city_country);
+
+        TextView T = findViewById(R.id.textView2);
+        Temperature = -14;
+        T.setText(getResources().getString(R.string.text_degrees, Temperature));
+
+        final TextView WType = findViewById(R.id.textView3);
+        WType.setText(R.string.WT);
+
+
+        final Button button = findViewById(R.id.theButton);
+         button.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+                 // Code here executes on main thread after user presses button
+                 WType.setText("Snow again");
+             }
+         });
+
+
     }
 }
