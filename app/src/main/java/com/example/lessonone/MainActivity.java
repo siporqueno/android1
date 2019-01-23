9ad258851c97a35d855f5b1d9561dac7c111e03b
siporqueno <strike1000001@gmail.com>
2019-01-24 00:00:58 +0300

    Changed type of variable temperature from int to double.
    Changed d for f in the placeholder.

--- app/src/main/java/com/example/lessonone/MainActivity.java
+++ app/src/main/java/com/example/lessonone/MainActivity.java
@@ -8,25 +8,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int temperature;
+    double temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView place = findViewById(R.id.textView1);
+        TextView place = findViewById(R.id.PLACE);
         place.setText(R.string.city_country);
 
-        TextView T = findViewById(R.id.textView2);
-        temperature = -14;
+        TextView T = findViewById(R.id.T);
+        temperature = -14.5;
         T.setText(getResources().getString(R.string.text_degrees, temperature));
 
-        final TextView WType = findViewById(R.id.textView3);
+        final TextView WType = findViewById(R.id.W_TYPE);
         WType.setText(R.string.WT);
 
 
-        final Button button = findViewById(R.id.theButton);
+        final Button button = findViewById(R.id.refresh_button);
          button.setOnClickListener(new View.OnClickListener() {
              public void onClick(View v) {
                  // Code here executes on main thread after user presses button
